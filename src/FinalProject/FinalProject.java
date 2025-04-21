968745891279a849f292401d9b24ea9f644372bc
Joshua Candia <joshuacandia74@gmail.com>
2025-04-21 11:28:11 -0300

    improve code for more legibility

--- src/FinalProject/FinalProject.java
+++ src/FinalProject/FinalProject.java
@@ -4,30 +4,26 @@ import java.util.Scanner;
 
 public class FinalProject {
     public static void main(String[] args) {
-        int age, grade1, grade2, grade3;
-        double average;
-        String newData;
         boolean repeat = true;
-
         Scanner scanner = new Scanner(System.in);
 
         do {
             System.out.println("Ingrese su edad:");
-            age = scanner.nextInt();
+            int age = scanner.nextInt();
 
             if (age < 18) {
                 System.out.println("Es menor de edad, vuelva cuando cumpla 18.");
             } else {
                 System.out.println("Ingrese la primera calificación:");
-                grade1 = scanner.nextInt();
+                int grade1 = scanner.nextInt();
 
                 System.out.println("Ingrese la segunda calificación:");
-                grade2 = scanner.nextInt();
+                int grade2 = scanner.nextInt();
 
                 System.out.println("Ingrese la tercera calificación:");
-                grade3 = scanner.nextInt();
+                int grade3 = scanner.nextInt();
 
-                average = (grade1 + grade2 + grade3) / 3.0;
+                double average = (grade1 + grade2 + grade3) / 3.0;
 
                 if (average >= 6) {
                     System.out.printf("Aprobado con: %.2f%n", average);
@@ -37,7 +33,7 @@ public class FinalProject {
             }
 
             System.out.println("¿Desea ingresar nuevos datos? Escriba 'si' o 'no':");
-            newData = scanner.next();
+            String newData = scanner.next();
 
             if (!newData.equalsIgnoreCase("si")) {
                 repeat = false;
